fd85c907847db3c6ecafaccda3701a01068e510c
Clo Yun-Hee Dufour <cloyunhee@gmail.com>
2022-04-12 11:21:15 +0200

    Clamp heatmap texture to avoid wrapping

--- backends/implot_impl_opengl3.cpp
+++ backends/implot_impl_opengl3.cpp
@@ -64,6 +64,8 @@ struct HeatmapData
 	HeatmapData() {
 		glGenTextures(1, &HeatmapTexID);
 		glBindTexture(GL_TEXTURE_2D, HeatmapTexID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glBindTexture(GL_TEXTURE_2D, 0);
